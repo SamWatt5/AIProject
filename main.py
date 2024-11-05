a0d8cc04498a388dc0ab0bbfb8758e85102d1284
SamWatts5 <62394214+SamWatts5@users.noreply.github.com>
2024-11-05 11:12:41 +0000

    Refactor grid handling and add movement functionality

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import random
 def main():
     grid = createGrid(3)
     randomizeGrid(grid)
-    print(grid)
+    printGrid(grid)
+    turn(grid, 1, 1)
 
 
 def createGrid(size):
@@ -12,19 +13,59 @@ def createGrid(size):
     counter = 0
     for i in range(size):
         for j in range(size):
-            grid[i][j] = counter
+            if counter == 0:
+                grid[i][j] = " "
+            else:
+                grid[i][j] = counter
             counter += 1
 
     return grid
 
 
 def randomizeGrid(grid):
-    random.shuffle(grid)
+    gridContents = []
+    for i in range(len(grid)):
+        for j in range(len(grid)):
+            gridContents.append(grid[i][j])
+    random.shuffle(gridContents)
+    counter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid)):
+            grid[i][j] = gridContents[counter]
+            counter += 1
 
 
 def printGrid(grid):
-    for i in range(grid.size()):
-        print(grid[i] + "\n")
+    for row in grid:
+        print(" ".join(f"{tile}" for tile in row))
+
+    print("")
+
+
+def check(grid, x, y):
+    print("checked grid[" + str(x) + "][" + str(y) + "] = " + str(grid[x][y]))
+    if grid[y][x] == " ":
+        return True
+    return False
+
+
+def turn(grid, x, y):
+    # up x y-1
+    if check(grid, x, y-1):
+        print("up")
+    # down x y+1
+    elif check(grid, x, y+1):
+        print("down")
+    # left x-1 y
+    elif check(grid, x-1, y):
+        # do something
+        print("left")
+    # right x+1 y
+    elif check(grid, x+1, y):
+        print("right")
+        # do something
+    else:
+        print("cant move")
 
 
 main()
