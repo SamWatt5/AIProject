0dc89fe368b130ea7aea37f8f535693f25d6e29d
Dylan <Dylanmwakasekele@gmai.com>
2024-11-15 13:15:26 +0000

    dependecies script for mac and linux

---
+++ mac-linux-dependencies.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+
+echo "Creating virtual environment..."
+python3 -m venv .venv
+
+echo "Activating virtual environment..."
+source .venv/bin/activate  
+
+echo "Installing pip..."
+python3 -m pip install --upgrade pip
+python3 -m pip --version
+
+echo "Installing packages..."
+python3 -m pip install pandas pillow tk
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to install packages."
+    exit 1
+fi
+
+echo "All packages installed successfully!"
+
+read -p "Press any key to continue..."
