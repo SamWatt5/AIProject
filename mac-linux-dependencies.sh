8596b024529f7d36f1122895e357f75d9462ae8e
Dylan <Dylanmwakasekele@gmai.com>
2024-11-15 13:38:59 +0000

    SSL certificates verification for mac

--- mac-linux-dependencies.sh
+++ mac-linux-dependencies.sh
@@ -12,11 +12,18 @@ python3 -m pip --version
 
 echo "Installing packages..."
 python3 -m pip install pandas pillow tk
+
+echo "Ensuring that SSL certificates are properly configured"
+
+python3 -m pip install certifi
+
 if [ $? -ne 0 ]; then
     echo "Error: Failed to install packages."
     exit 1
 fi
 
 echo "All packages installed successfully!"
+
+python3 -m pip install certifi
 
 read -p "Press any key to continue..."
