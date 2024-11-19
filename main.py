81cc2145afeac93166f35cedb5e8e4976d43017f
SamWatts5 <2508240@dundee.ac.uk>
2024-11-19 11:47:42 +0000

    refactor: remove unused movie search and related print statement

--- main.py
+++ main.py
@@ -23,11 +23,11 @@ removed_movies = []
 def main():
     graph = MovieGraph(df)
     starting_movie = search()
-    other_movie = search()
+    # other_movie = search()
 
-    print(f"{graph.get_p_cost(starting_movie, other_movie)}")
+    # print(f"{graph.get_p_cost(starting_movie, other_movie)}")
 
-# display_movie(starting_movie)
+    display_movie(starting_movie)
 
 
 def display_movie(movie_title):
