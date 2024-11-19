c2bab8ff8dab956763af0822f5a90920c010a839
SamWatts5 <2508240@dundee.ac.uk>
2024-11-19 16:15:32 +0000

    Refactor Node class for improved readability and structure

--- search.py
+++ search.py
@@ -20,48 +20,47 @@ class Problem:
         # return cost of a solution path that arrives at state2 from state1 via action, default cost = 1,
         return cost+1
 
-    class Node:
-        def __init__(self, state, parent=None, action=None, path_cost=0):
-            self.state = state
-            self.parent = parent
-            self.action = action
-            self.path_cost = path_cost
-            self.depth = 0
-            if parent:
-                self.depth = parent.depth + 1
 
-        def child_node(self, problem, action):
-            next_state = problem.result(self.state, action)
-            next_node = Node(next_state, self, action, problem.path_cost(
-                self.path_cost, self.state, action, next_state))
-            return next_node
+class Node:
+    def __init__(self, state, parent=None, action=None, path_cost=0):
+        self.state = state
+        self.parent = parent
+        self.action = action
+        self.path_cost = path_cost
+        self.depth = 0
+        if parent:
+            self.depth = parent.depth + 1
 
-        def expand(self, problem, action):
-            # get list of all possible states from this state
-            children = []
-            for action in problem.actions(self.state):
-                children.append(self.child_node(problem, action))
-            return children
+    def child_node(self, problem, action):
+        next_state = problem.result(self.state, action)
+        next_node = Node(next_state, self, action, problem.path_cost(
+            self.path_cost, self.state, action, next_state))
+        return next_node
 
-        def path(self):
-            # find a path of nodes to this node
-            node = self
-            path = []
-            while node:
-                path.append(node)
-                node = node.parent
-            return list(reversed(path))
+    def expand(self, problem, action):
+        # get list of all possible states from this state
+        children = []
+        for action in problem.actions(self.state):
+            children.append(self.child_node(problem, action))
+        return children
 
-        def solution(self):
-            # return every action in the path
-            actions = []
-            for node in self.path()[1:]:
-                actions.append(node.action)
-            return actions
+    def path(self):
+        # find a path of nodes to this node
+        node = self
+        path = []
+        while node:
+            path.append(node)
+            node = node.parent
+        return list(reversed(path))
+
+    def solution(self):
+        # return every action in the path
+        actions = []
+        for node in self.path()[1:]:
+            actions.append(node.action)
+        return actions
 
 
 class Frontier:
     def __init__(self):
         pass
-
-    
