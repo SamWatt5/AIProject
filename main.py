179106960fe63ba8e54f9443749d40be213de422
SamWatts5 <2508240@dundee.ac.uk>
2024-11-26 14:28:07 +0000

    refactor main.py to implement movie result display and create_movie_box function

--- main.py
+++ main.py
@@ -32,35 +32,85 @@ def main():
     starting_movie_title = search()
     # starting_movie = df[df["Title"] == starting_movie_title]
 
-    problem = Problem(starting_movie_title, graph, 10)
-    print(problem.actions(starting_movie_title))
+    # problem = Problem(starting_movie_title, graph, 10)
+    # print(problem.actions(starting_movie_title))
 
-    problem.do_searches(starting_movie_title)
+    # results = problem.do_searches(starting_movie_title)
+    results = [search() for _ in range(3)]
+    display_results(starting_movie_title, results)
 
-    # other_movie = search()
 
-    # movie1 = df[df["Title"] == starting_movie]
-    # directors1 = movie1["Director"].iloc[0].split(", ")
-    # cast1 = movie1["Cast"].iloc[0].split(", ")
-    # genres1 = movie1["Genre"].iloc[0].split(", ")
+def display_results(startingMovie, results):
+    window = tk.Tk()
+    window.configure(bg="#caf0f8")
+    starting_box = create_movie_box(startingMovie)
+    boxes = []
+    for movie in results:
+        boxes.append(create_movie_box(movie))
 
-    # movie2 = df[df["Title"] == other_movie]
-    # directors2 = movie2["Director"].iloc[0].split(", ")
-    # cast2 = movie2["Cast"].iloc[0].split(", ")
-    # genres2 = movie2["Genre"].iloc[0].split(", ")
+    starting_box.grid(row=0, column=1, padx=10, pady=10)
 
-    # print(f"Movie 1 - Genres: {genres1}, Directors: {
-    #       directors1}, Cast: {cast1}")
+    for i, box in enumerate(boxes):
+        r = i // 3
+        c = i % 3
+        box.grid(row=r+1, column=c, padx=10, pady=10)
 
-    # print(f"Movie 2 - Genres: {genres2}, Directors: {
-    #     directors2}, Cast: {cast2}")
+    # largestWidth = 0
+    # for box in boxes:
+    #     if box.winfo_width() > largestWidth:
+    #         largestWidth = box.winfo_width()
 
-    # print(f"genre: {graph.genre_path_cost(set(genres1), set(genres2))}\n"
-    #       f"director: {graph.director_path_cost(
-    #           set(directors1), set(directors2))}\n"
-    #       f"cast: {graph.cast_path_cost(set(cast1), set(cast2))}\n")
+    # starting_box.configure(width=largestWidth)
+    # for box in boxes:
+    #     box.configure(width=largestWidth)
 
-    # display_movie(starting_movie)
+    window.mainloop()
+
+
+def create_movie_box(movie_title):
+    print("\nDisplaying Movie.....\n")
+    movie = df[df["Title"] == movie_title]
+    director = movie["Director"].iloc[0]
+    cast = movie["Cast"].iloc[0].split(", ")[:3]
+    genres = movie["Genre"].iloc[0].split(", ")
+
+    print("Title:\n  " + movie_title + "\n")
+    print("Director:\n  " + director + "\n")
+    print("Cast:")
+    for actor in cast:
+        print("  " + actor)
+    print("")
+    print("Genres:")
+    for genre in genres:
+        print("  " + genre)
+
+    poster_url = movie["Poster"].iloc[0]
+    with urllib.request.urlopen(poster_url) as u:
+        raw_data = u.read()
+    image = Image.open(io.BytesIO(raw_data))
+    root = tk.Frame(bg="#90e0ef", width=300)
+    photo = ImageTk.PhotoImage(image)
+    poster_label = tk.Label(root, image=photo)
+    title_label = tk.Label(root, text=movie_title,
+                           font="tkHeadingFont", bg="#90e0ef")
+    director_label = tk.Label(root, text=(
+        "Directed by: " + director), bg="#90e0ef")
+    actor_labels = [tk.Label(root, text="Cast:", bg="#90e0ef")]
+    for actor in cast:
+        actor_labels.append(tk.Label(root, text=(actor), bg="#90e0ef"))
+    genre_labels = [tk.Label(root, text="Genres:", bg="#90e0ef")]
+    for genre in genres:
+        genre_labels.append(tk.Label(root, text=(genre), bg="#90e0ef"))
+
+    poster_label.pack()
+    title_label.pack()
+    director_label.pack()
+    for label in actor_labels:
+        label.pack()
+    for label in genre_labels:
+        label.pack()
+
+    return root
 
 
 def display_movie(movie_title):
